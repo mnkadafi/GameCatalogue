94ab373f34c42dc00ad8e6032306987a37f9dca1
Mochamad Nurkhayal Kadafi <wowkadafi@gmail.com>
2020-07-07 20:23:26 +0700

    Initial Commit

---
+++ GameCatalogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCatalogue
+//
+//  Created by Mochamad Nurkhayal Kadafi on 07/07/20.
+//  Copyright © 2020 Mochamad Nurkhayal Kadafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
