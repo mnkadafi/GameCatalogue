5799e91fab4469c74c6e7971eb2343daae7542f9
Mochamad Nurkhayal Kadafi <wowkadafi@gmail.com>
2020-07-11 17:39:52 +0700

    test

---
+++ GameCatalogue/Controller/GamesCell.swift
@@ -0,0 +1,20 @@
+//
+//  GamesCell.swift
+//  GameCatalogue
+//
+//  Created by Mochamad Nurkhayal Kadafi on 09/07/20.
+//  Copyright © 2020 Mochamad Nurkhayal Kadafi. All rights reserved.
+//
+
+import UIKit
+
+class GamesCell: UITableViewCell {
+
+    @IBOutlet weak var gameImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var platformLabel: UILabel!
+    
+}
